c0a32d877b44194dcdc0f796ec0a6d7b6db47d72
rogers <lubangarogers733@gmail.com>
2019-08-20 17:31:31 +0300

    Adding image slider and updating card content

--- static/js/index.js
+++ static/js/index.js
@@ -9,3 +9,43 @@ let hideBox = ()=>{
     })
 
 }
+
+//image slider rules
+var slideIndex = 1;
+//invoking the showSlides function
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+// show slides function to make the images slide
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length} ;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].classList.add("active");
+}
+class human{
+constructor(name, age){
+this.name = name;
+this.age= age;
+
+}
+talk(){
+
+}
+
+}
